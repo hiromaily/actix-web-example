79aa962238b8ca3c90baaa6dffd669f5a4a37125
HirokiYasui <hiromaily2@gmail.com>
2024-06-14 12:41:06 +0900

    update database schema

--- docker/pg/init.sql
+++ docker/pg/init.sql
@@ -2,5 +2,22 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TYPE todo_status AS ENUM ('pending', 'doing', 'canceled', 'done');
+
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description TEXT,
+    status todo_status NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+        REFERENCES users(id)
 );
