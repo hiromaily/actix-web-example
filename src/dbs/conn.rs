ab64107ebe6d6d7ddbe2aa6313fc2aa78c574369
HirokiYasui <hiromaily2@gmail.com>
2024-06-17 22:00:37 +0900

    add timeout for db connection

--- src/dbs/conn.rs
+++ src/dbs/conn.rs
@@ -1,4 +1,5 @@
-use sea_orm::{Database, DbErr};
+use sea_orm::{ConnectOptions, Database, DbErr};
+use std::time::Duration;
 
 // refer to: https://www.sea-ql.org/sea-orm-tutorial/ch01-01-project-setup.html
 pub async fn get_conn(
@@ -8,7 +9,19 @@ pub async fn get_conn(
     db_name: &str,
 ) -> Result<sea_orm::DatabaseConnection, DbErr> {
     let db_url = format!("postgresql://{user}:{password}@{host}/{db_name}");
-    let conn: sea_orm::DatabaseConnection = Database::connect(db_url).await?;
+    let mut opt = ConnectOptions::new(db_url);
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(10))
+        .acquire_timeout(Duration::from_secs(10))
+        .idle_timeout(Duration::from_secs(10))
+        .max_lifetime(Duration::from_secs(10))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+    //.set_schema_search_path("my_schema"); // Setting default PostgreSQL schema
+    let conn = Database::connect(opt).await?;
+
+    //let conn: sea_orm::DatabaseConnection = Database::connect(db_url).await?;
     // let db = &match conn.get_database_backend() {
     //     DbBackend::Postgres => {
     //         db.execute(Statement::from_string(
